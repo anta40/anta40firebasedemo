88c47b50a0d89e30bcd15821023d2d11c939d6e7
Andre Tampubolon <andre@lc.vlsm.org>
2017-07-13 10:44:27 +0700

    First commit

---
+++ android/app/src/main/java/net/devbyzero/app/anta40firebase/Config.java
@@ -0,0 +1,21 @@
+package net.devbyzero.app.anta40firebase;
+
+/**
+ * Created by anta40 on 12-Jul-17.
+ */
+
+public class Config {
+
+    // global topic to receive app wide push notifications
+    public static final String TOPIC_NEWS = "/topics/news";
+
+    // broadcast receiver intent filters
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+    public static final String PUSH_NOTIFICATION = "pushNotification";
+
+    // id to handle the notification in the notification tray
+    public static final int NOTIFICATION_ID = 100;
+    public static final int NOTIFICATION_ID_BIG_IMAGE = 101;
+
+    public static final String SHARED_PREF = "ah_firebase";
+}
